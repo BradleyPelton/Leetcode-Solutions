17f92498c95ad47e57e381b77ae418dc339f92fd
unknown <bradleypelton@gmail.com>
2020-05-02 23:57:59 -0400

    leetcode20200502

---
+++ leetcode20200502/kposition.py
@@ -0,0 +1,26 @@
+from typing import List
+# Accepted first attempt after 9 minutes. Not the prettiest of solutions but it works
+class Solution:
+    def kLengthApart(self, nums: List[int], k: int) -> bool:
+        """ """
+
+        index_list = []
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                index_list.append(i)
+        print(index_list)
+
+        for j in range(len(index_list)-1):
+            if index_list[j+1] - index_list[j] < k+1:
+                print(f"found a problem with indexes {index_list[j], index_list[j+1]}")
+                print(False)
+                return(False)
+        print(True)
+        return(True)
+
+
+a = Solution()
+# a.kLengthApart([1,0,0,0,1,0,0,1],2)
+# a.kLengthApart([1,0,0,1,0,1],2)
+# a.kLengthApart([1,1,1,1,1],0)
+a.kLengthApart([0,1,0,1],1)
