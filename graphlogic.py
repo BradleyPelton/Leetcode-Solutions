fa4d9fb862755d844905e3f3381d0cbf0885bac0
unknown <bradleypelton@gmail.com>
2020-05-05 12:05:02 -0400

    adding charts, formating readme

---
+++ graphlogic.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import matplotlib.dates as mdates
+import datetime
+
+
+results = [
+    ['05/02', 59.783],
+    ['05/02', 36.147],
+    ['04/25', 55.634],
+    ['04/18', 79.905],
+    ['04/18', 32.082],
+    ['04/11', 78.335],
+    ['04/04', 61.380],
+    ['04/04', 45.532],
+    ['03/28', 76.434],
+    ['03/21', 68.548],
+    ['03/21', 32.919],
+    ['03/14', 47.413],
+    ['03/07', 48.908],
+    ['03/07', 56.330],
+    ['02/29', 51.688],
+    ['02/22', 58.655],
+    ['02/22', 32.819],
+    ['02/15', 59.215],
+    ['02/08', 33.405],
+    ['02/08', 33.929],
+    ['02/01', 40.488]
+]
+
+dates = [result[0] for result in results]
+dates_formated = [datetime.datetime.strptime(date + '/2020', "%m/%d/%Y").date() for date in dates]
+
+percentiles = [result[1] for result in results]
+
+plt.plot(dates_formated, percentiles)
+plt.xlabel('competition date')
+plt.ylabel('Rank(Percentile)')
+plt.legend(["Rank(as a percentile)"])
+plt.axhline(y=50, color='r', linestyle='-')
+axes = plt.gca()
+# axes.set_xlim([xmin,xmax])
+axes.xaxis.set_major_locator(mdates.DayLocator(interval=21))
+axes.set_ylim([0, 100])
+plt.show()
