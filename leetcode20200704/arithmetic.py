c61276fe05b600d45208a46a59ca84c612601093
unknown <bradleypelton@gmail.com>
2020-07-04 23:41:31 -0400

    leetcode20200704

---
+++ leetcode20200704/arithmetic.py
@@ -0,0 +1,22 @@
+from typing import List
+
+## Accepted after 3 minutes. Not even sure how my solution worked, but hey, it worked
+
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        """ """
+        arr.sort()
+
+        diff = [arr[i] - arr[i+1] for i in range(len(arr)-1)]
+
+        print(diff)
+
+        if len(list(set(diff))) == 1:
+            print("trueee")
+            return True
+        else:
+            return False
+
+a = Solution()
+# a.canMakeArithmeticProgression([3,5,1])
+a.canMakeArithmeticProgression([1,2,4])
